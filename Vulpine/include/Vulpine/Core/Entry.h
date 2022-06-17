f474f000a4bd3094b7aa64e149b06cbba638a740
Alex Dauncey <84759985+Shylexx@users.noreply.github.com>
2022-06-17 15:58:07 +0100

    GLFW+GLM includes + Basic GLFW Window

--- Vulpine/include/Vulpine/Core/Entry.h
+++ Vulpine/include/Vulpine/Core/Entry.h
@@ -3,6 +3,7 @@
 
 #include "GameApp.h"
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 extern Vulpine::GameApp *Vulpine::CreateApp();
 
@@ -11,9 +12,24 @@ int main(int argc, char **argv)
     std::cout << "Hello Vulpine!" << std::endl;
     Vulpine::GameApp::PrintHelloWorld();
 
+    if(!glfwInit())
+    {
+        return 1;
+    }
+
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Vulpine Engine", NULL, NULL);
+
+    if (!window) {
+        std::cout << "Window Creation Failed" << std::endl;
+    }
+
     auto app = Vulpine::CreateApp();
     app->Run();
     delete app;
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
 }
