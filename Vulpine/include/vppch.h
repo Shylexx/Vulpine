6b61dcff27db5d8beefb8c4e13909295eab504e5
Alex Dauncey <84759985+Shylexx@users.noreply.github.com>
2022-06-21 13:26:58 +0100

    Precompiled Headers

---
+++ Vulpine/include/vppch.h
@@ -0,0 +1,24 @@
+#ifndef __VP_PCH__
+#define __VP_PCH__
+
+// types
+#include <string>
+#include <sstream>
+
+// containers
+#include <map>
+#include <unordered_map>
+#include <iterator>
+#include <vector>
+#include <set>
+#include <unordered_set>
+
+// util
+#include <memory>
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <cmath>
+
+
+#endif
