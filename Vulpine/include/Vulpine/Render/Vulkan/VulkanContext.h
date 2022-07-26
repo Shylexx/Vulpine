6d396b0f40dac3fedef3da4817bca0cff0f5c796
Shylexx <alexanderdauncey@gmail.com>
2022-07-26 16:08:00 +0100

    Vk Context Header

---
+++ Vulpine/include/Vulpine/Render/Vulkan/VulkanContext.h
@@ -0,0 +1,17 @@
+#ifndef __VK_CONTEXT_H__
+#define __VK_CONTEXT_H__
+
+#include <vulkan/vulkan.h>
+
+namespace Vulpine
+{
+  class VulkanContext
+  {
+  public:
+
+  private:
+    VkDevice m_LogicalDevice;
+  };
+}
+
+#endif
